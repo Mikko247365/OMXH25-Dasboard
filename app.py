b0d16850ac3f4b268d42838c30104f292bfbe6d3
Mikko247365 <mikko.j.keinanen@gmail.com>
2026-06-24 19:25:48 +0300

    Lisäsin vielä yhden setin tähän

--- app.py
+++ app.py
@@ -5,3 +5,4 @@ import yfinance as yf
 st.set_page_config(page_title="OMXH25 Dashboard", layout="wide")
 st.title("OMXH25 Dashboard")
 st.write("Projektin ensimmäinen runkoversio.")
+st.write("Projektin Toinen runkoversio.")
